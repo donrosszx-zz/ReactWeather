6ddd405718c2846edabea98264e43f16bbfeac75
donrosszx <donrosszx@gmail.com>
2016-06-27 16:35:21 -0500

    about links target to new page or tab

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,9 +6,9 @@ var About = (props) => {
             <h1 className="text-center">About</h1>
             <p>This is my first React.js project using Foundation</p>
             <ul>
-                <li><a href="http://foundation.zurb.com/docs">Foundation Docs</a></li>
-                <li><a href="https://github.com/donrosszx/ReactWeather">My github repo</a></li>
-                <li><a href="http://openweathermap.org/current">Open Weather Map API</a></li>
+                <li><a href="http://foundation.zurb.com/docs" target="_blank">Foundation Docs</a></li>
+                <li><a href="https://github.com/donrosszx/ReactWeather" target="_blank">My github repo</a></li>
+                <li><a href="http://openweathermap.org/current" target="_blank">Open Weather Map API</a></li>
             </ul>
         </div>
     )
