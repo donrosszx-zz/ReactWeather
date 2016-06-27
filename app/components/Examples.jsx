7fd54876e340456b5bf7e54479567ac1a4899644
donrosszx <donrosszx@gmail.com>
2016-06-27 15:52:39 -0500

    styling the grid on the main component and examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>This is the examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li><Link to="/?location=Nashville">Nashville, TN</Link></li>
+                <li><Link to="/?location=Phoenix">Phoenix, AZ</Link></li>
+            </ol>
         </div>
     )
 };
